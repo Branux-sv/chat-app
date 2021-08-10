8445ae3c194b07bf723f4d27b744a0c9de574302
Brayan Marroquin <eduar.gol07@gmail.com>
2021-08-09 23:44:58 -0600

    Setting up socket.io module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 //Define public path
@@ -13,7 +17,11 @@ app.get('/', (req, resp) => {
     resp.render('index')
 })
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New WebSocket Connection')
+})
+
+server.listen(port, () => {
     console.log('Server is running up in port ' + port)
 })
 
